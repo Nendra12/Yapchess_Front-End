04dad82c8167dd24c4c6d4bf109f7d3c5fd30c2a
zanra2401 <grubgrex@gmail.com>
2025-09-28 20:42:32 +0700

    update

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,4 +1,4 @@
-// import "../styles/topbar.css"
+import "../styles/utils.css"
 import { Menubar } from "primereact/menubar";
 import { Card } from "primereact/card";
 import { Image } from "primereact/image";
@@ -20,18 +20,30 @@ function TopBar() {
             <Image src={logo} width="100" />
         </a>
     );
+
+    const justifStart = {
+        justifyContent: "start",
+    }
     
     const end = (
-        <Card className="p-1">
+        <div>
             <div className="flex align-items-center gap-2">
-                <Avatar image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png" shape="square" />
-                <h3>Username</h3>
+                <Avatar label="R" size="xlarge" shape="square"  />
+                <div style={{
+                    display: "flex",
+                    alignItems: "start",
+                    flexDirection: "column",
+                    justifyContent: "start",
+                }}>
+                    <span className="m-0 font-bold bg-cyan-300">Username</span>
+                    <span className="m-0 text-100 text-sm">ELO</span>
+                </div>
             </div>
-        </Card>
+        </div>
     );
 
     return (
-        <Menubar className="p-1" start={logoItem} end={end} />
+        <Menubar className="p-2 px-7 m-0 bg-" start={logoItem} end={end} />
     );
 }
 
