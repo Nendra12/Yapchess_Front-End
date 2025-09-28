0a6b866f7b5ba188201c6ebfcf6ab107430a5a6a
zanra2401 <grubgrex@gmail.com>
2025-09-28 19:29:49 +0700

    topbar progress

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Image } from "primereact/image"
+import Logo from "../assets/logo.png"
+import TopBar from "../components/TopBar"
+
+function HomePage() {
+    return (
+        <TopBar/>
+    );
+}
+
+export default HomePage;
